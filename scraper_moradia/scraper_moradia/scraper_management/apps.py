74f39baefcad84ead0cb1888bb01ed7f0daae0f4
JeanRabelo <jeanpablosousarabelo@gmail.com>
2025-05-15 23:38:14 -0300

    fazendo um scraper básico

---
+++ scraper_moradia/scraper_moradia/scraper_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ScraperManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'scraper_management'
---
+++ scraper_moradia/scraper_moradia/scraper_management/scrapers.py
@@ -0,0 +1,32 @@
+import undetected_chromedriver as uc
+import time
+import sys
+
+# Configure Chrome options and initialize the driver
+options = uc.ChromeOptions()
+driver = uc.Chrome(
+    options=options,
+    driver_executable_path="/home/jean/Github/JeanRabelo/Júlia/scraper_moradia/chromedriver"
+)
+
+# URLs for login and post-login detection
+login_url = "https://www.quintoandar.com.br/auth/login"
+post_login_url = "https://www.quintoandar.com.br/"
+
+# Navigate to the login page
+driver.get(login_url)
+print(f"Please log in at {login_url}...")
+
+# Wait for the user to complete login by checking the current URL
+while True:
+    current_url = driver.current_url
+    if current_url == post_login_url:
+        break
+    time.sleep(1)
+
+# Once logged in, notify the user, wait a bit, then clean up
+print("User logged in")
+time.sleep(15)
+driver.quit()
+sys.exit()
+
